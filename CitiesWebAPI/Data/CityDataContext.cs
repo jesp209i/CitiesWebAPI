4ebef21f005f69fef08257e0c6735afa50f3a745
Christian Schou <christian.schou17@gmail.com>
2018-11-06 10:38:34 +0100

    Added data to database and parameter for creating tables from properties

--- CitiesWebAPI/Data/CityDataContext.cs
+++ CitiesWebAPI/Data/CityDataContext.cs
@@ -15,7 +15,15 @@ namespace CitiesWebAPI.Data
         public CityDataContext(DbContextOptions<CityDataContext> options) 
             : base(options)
         {
-            
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
         }
     }
 }

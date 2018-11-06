d1414775e87fe2227204bad66aa91965adc2c73f
Christian Schou <christian.schou17@gmail.com>
2018-11-06 10:38:53 +0100

    Added validation when adding the new City

--- CitiesWebAPI/Models/City.cs
+++ CitiesWebAPI/Models/City.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace CitiesWebAPI.Models
     public class City
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
 
+        [Required]
+        [MinLength(10)]
         public string Description { get; set; }
 
         public List<Place> Places { get; set; }

5639b36c6e0871ddc4963c444707500b03cebd82
Christian Schou <christian.schou17@gmail.com>
2018-11-06 10:37:58 +0100

    Added controller for returning place information

--- CitiesWebAPI/Controllers/PlaceController.cs
+++ CitiesWebAPI/Controllers/PlaceController.cs
@@ -5,15 +5,38 @@ using System.Threading.Tasks;
 using CitiesWebAPI.Data;
 using CitiesWebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CitiesWebAPI.Controllers
 {
-    public class PlaceController : Controller
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class PlaceController : ControllerBase
     {
-       // GET: /<controller>/
-        public IActionResult Index()
+        private readonly CityDataContext _db;
+        public PlaceController(CityDataContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        [Route("Places/{cityId}")]
+        public IActionResult GetPlaces(int cityId)
+        {
+            List<Place> places = _db.Places.Where(x => x.CityId == cityId).ToList();
+
+            return new ObjectResult(places);
+        }
+
+        [Route("City/{cityId}/place/{placeId}")]
+        public IActionResult GetCity(int cityId, int placeId)
+        {
+
+            if (!_db.Places.ToList().Exists(x => x.CityId == cityId && x.Id == placeId))
+            {
+                return NotFound();
+            }
+            return new ObjectResult(_db.Places.ToList().FindAll(x => x.CityId == cityId && x.Id == placeId));
+
         }
     }
 }
